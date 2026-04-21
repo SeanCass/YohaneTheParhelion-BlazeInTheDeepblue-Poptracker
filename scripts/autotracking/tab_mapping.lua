dd2afec1f43b3c7f0c5a187b57958d3d5db0711a
Sean <seancasssrl@gmail.com>
2026-04-21 18:09:14 -0400

    added tab_mapping.lua

---
+++ scripts/autotracking/tab_mapping.lua
@@ -0,0 +1,13 @@
+TAB_MAPPING = {
+    [0] = {"Mysterious Dungeon"},
+    [1] = {"Sunken Temple"},
+    [2] = {"Ruins"},
+    [3] = {"Grotto"}
+	[4] = {"Coral Hill"},
+    [5] = {"Sea of Trees"},
+    [6] = {"Crystalline Grotto"},
+    [7] = {"Sunken Volcano"},
+	[8] = {"Shipwreck"},
+	[9] = {"Infernal Altar"},
+	[10] = {"Aqours Memoria"},
+}
